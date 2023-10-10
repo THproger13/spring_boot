90c03023a6ddca54caea931ca1ffa623054d4576
THproger13 <thphysics@naver.com>
2023-10-10 10:24:17 +0900

    프로젝트 재셋업 위한 백업 푸시

---
+++ MemberBoard_20231006/src/test/java/com/example/memberboard/MemberBoard20231006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memberboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberBoard20231006ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
